a0f1dad24b55e4491efca8ef66806142bdffbb3a
E.Alamo <erosdelalamo@gmail.com>
2025-08-25 16:58:16 +0200

    Added form tag surrounding upload button to fix functional tests
    Signed-off-by: E.Alamo <erosdelalamo@gmail.com>

--- src/widgets/views/uploadButton.php
+++ src/widgets/views/uploadButton.php
@@ -16,12 +16,12 @@ $label = $isVersion ? Yii::t('filescatalog', 'Add version') : Yii::t('filescatal
 
 <span class="btn btn-default fileinput-button" data-toggle="tooltip" data-container="body" title="<?= $label ?>">
     <i class="<?= $addFilesIconClass ?>"></i>
-   <span>
+   <form><span>
        <?php /** @var boolean $showLabels */
        if ($showLabels || $isVersion): ?>
            <?= $label ?>
        <?php endif; ?>
-   </span>
+   </span></form>
     <!-- The file input field used as target for the file upload widget -->
     <?= $input ?>
 </span>
